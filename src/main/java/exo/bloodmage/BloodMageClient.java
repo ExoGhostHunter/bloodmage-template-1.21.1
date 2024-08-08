fa1cb9c09253a22db5f8ab4c5f0cbea49ce19969
ExoGhostHunter <mcjordy0055@gmail.com>
2024-08-08 22:57:35 +0200

    Initial commit

---
+++ src/main/java/exo/bloodmage/BloodMageClient.java
@@ -0,0 +1,10 @@
+package exo.bloodmage;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BloodMageClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/exo/bloodmage/Bloodmage.java
@@ -0,0 +1,17 @@
+package exo.bloodmage;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Bloodmage implements ModInitializer {
+    public static final String MOD_ID = "bloodmagemod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
